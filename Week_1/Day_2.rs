54b9ed4748a47730ce82d80a95af4afd0d14aab1
James Anaga <jamesanaga1@gmail.com>
2024-10-21 15:57:44 +0100

    Create Day_2.rs

---
+++ Week_1/Day_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Immutable variable
+    let age = 10; // Age is immutable by default
+    println!("Age = {}", age);
+
+    // Mutable variable
+    let mut mutable_age = 10; // Declare a mutable variable
+    mutable_age = 30; // Change the value of mutable_age
+}
+ 
