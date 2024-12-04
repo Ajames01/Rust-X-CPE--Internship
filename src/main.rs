12067ca5b04b9f575cb10f8b146ba25c0608da5f
James Anaga <jamesanaga1@gmail.com>
2024-12-04 14:18:35 +0100

    commit main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-              let my_string = String::from("The weather is \n nice \n outside! ");
-    
-        println!("{}",my_string.lines().iter());
+           println!("Welcome to My World");
     }
+
